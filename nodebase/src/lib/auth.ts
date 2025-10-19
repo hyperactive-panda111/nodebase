3a7e3232a8c9de168710930392083094278830bb
amomensah675 <amomensah675@gmail.com>
2025-10-19 06:32:29 +0000

    payments

--- nodebase/src/lib/auth.ts
+++ nodebase/src/lib/auth.ts
@@ -1,7 +1,9 @@
+import { checkout, polar, portal } from "@polar-sh/better-auth";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 // If your Prisma file is located elsewhere, you can change the path
 import prisma from '@/lib/db';
+import { polarClient } from './polar'
 
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
@@ -12,5 +14,23 @@ export const auth = betterAuth({
         autoSignIn: true,
     },
     trustedOrigins: ['https://3000-firebase-nodebase-1760493652353.cluster-fbfjltn375c6wqxlhoehbz44sk.cloudworkstations.dev'],
+    plugins: [
+        polar({
+            client: polarClient,
+            createCustomerOnSignUp: true,
+            use: [
+                checkout({
+                products: [
+                    {
+                        productId: '0ab49aff-3714-40d1-a7fa-e3afe24ad5ec',
+                        slug: 'nodebase',
+                    }
+                ],
+                successUrl: process.env.POLAR_SUCCESS_URL,
+                authenticatedUsersOnly: true,
+            }),
+             portal()]
+        }),
+    ]
 
 });
