f7ce1eadbc80f53183589f6b41ab4e0566b62229
amomensah675 <amomensah675@gmail.com>
2025-10-20 17:12:37 +0000

    workflows crud

--- nodebase/src/app/(dashboard)/(rest)/workflows/page.tsx
+++ nodebase/src/app/(dashboard)/(rest)/workflows/page.tsx
@@ -1,10 +1,25 @@
+import { WorkflowsContainer, WorkflowsList } from "@/features/workflows/components/workflows";
+import { prefetchWorkflows } from "@/features/workflows/server/prefetch";
 import { requireAuth } from "@/lib/auth-utils";
+import { HydrateClient } from "@/trpc/server";
+import { Suspense } from "react";
+import { ErrorBoundary } from 'react-error-boundary';
 
 const Page = async () => {
     await requireAuth();
     
+    prefetchWorkflows();
+
     return (
-        <p>Workflow page</p>
+        <WorkflowsContainer>
+            <HydrateClient>
+                <ErrorBoundary fallback={<p>Something went wrong.</p>}>
+                    <Suspense fallback={<p>Loading...</p>}>
+                        <WorkflowsList />
+                    </Suspense>
+                </ErrorBoundary>
+            </HydrateClient>
+        </WorkflowsContainer>
     );
 }
 
--- nodebase/src/trpc/routers/_app.ts
+++ nodebase/src/trpc/routers/_app.ts
@@ -1,31 +1,9 @@
-import prisma from '@/lib/db';
-import { createTRPCRouter,premiumProcedure,protectedProcedure } from '../init';
-import { inngest } from '@/inngest/client';
-import { google } from '@ai-sdk/google';
-import { generateText } from 'ai';
+import { createTRPCRouter } from '../init';
+import { workflowsRouter } from '@/features/workflows/server/routers';
 
 export const appRouter = createTRPCRouter({
-  testAi: premiumProcedure.mutation(async () => {
-    await inngest.send({
-      name: 'execute/ai',
-    });
+  workflows: workflowsRouter,
 
-    return { success: true, message: 'Job queued'}
-  }),
-  getWorflows: protectedProcedure.query(({ ctx }) => {
-      console.log({ userId: ctx.auth.user.id });
-      return prisma.workflow.findMany();
-  }),
-  createWorkflow: protectedProcedure.mutation(async () => {
-    await inngest.send({
-      name: 'test/hello.world',
-      data: {
-        email: 'test@ndc.com',
-      },
-    });
-
-    return { success: true, message: 'Job queued'}
-  }),
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
